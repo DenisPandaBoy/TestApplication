72d95f591d727daba77c7e57199cad52435d12ff
Denis <denis.fazik@demi.sk>
2025-08-11 09:16:25 +0200

    OrderItems init

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    /** @use HasFactory<\Database\Factories\OrderItemFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'count',
+        'price',
+        'vat',
+        'price_vat',
+    ];
+}
