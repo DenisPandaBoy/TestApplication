8841058a9a465e6f7dfe0d6231c316764a284ff9
Denis <denis.fazik@demi.sk>
2025-08-12 10:09:45 +0200

    Category migrations, Factories, Seeders

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Order;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,9 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Category::factory()
+            ->count(3)
+            ->has(Order::factory()->count(3))
+            ->create();
     }
 }
