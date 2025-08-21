77093564d2a93d0d471c8f448a4a0e4a8f206504
Denis <denis.fazik@demi.sk>
2025-08-21 11:25:29 +0200

    Return ids as well

--- app/Http/Resources/OrderItemResource.php
+++ app/Http/Resources/OrderItemResource.php
@@ -15,6 +15,7 @@ class OrderItemResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'order_id' => $this->order_id,
             'count' => $this->count,
